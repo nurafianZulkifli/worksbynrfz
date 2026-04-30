49a0231fdfcee3f58afae870512b01b6e960e28d
nrfz <63917225+nurafianZulkifli@users.noreply.github.com>
2026-04-30 23:49:01 +0800

    2026-05 Feature Update for worksbynrfz (270426.3)

--- js/app-version.js
+++ js/app-version.js
@@ -6,8 +6,22 @@
 
 window.APP_VERSIONS = {};
 
+// Get the script's directory
+function getScriptDirectory() {
+  const scripts = document.getElementsByTagName('script');
+  const currentScript = scripts[scripts.length - 1];
+  const src = currentScript.src;
+  if (src) {
+    return src.substring(0, src.lastIndexOf('/') + 1);
+  }
+  return window.location.origin + '/js/';
+}
+
+const scriptDir = getScriptDirectory();
+const versionJsonUrl = scriptDir + 'version.json';
+
 // Fetch version configuration
-fetch(new URL('../js/version.json', import.meta.url || location.href).href)
+fetch(versionJsonUrl)
   .then(response => response.json())
   .then(data => {
     window.APP_VERSIONS = data;
@@ -16,10 +30,11 @@ fetch(new URL('../js/version.json', import.meta.url || location.href).href)
     window.dispatchEvent(new CustomEvent('versionsLoaded', { detail: data }));
   })
   .catch(error => {
-    console.warn('[AppVersion] Could not load versions, using defaults:', error);
+    console.warn('[AppVersion] Could not load versions from ' + versionJsonUrl, error);
     // Fallback to defaults
     window.APP_VERSIONS = {
-      buszy: '4.5.3',
-      railbuddy: '4.5.3'
+      buszy: '4.5.x',
+      railbuddy: '4.5.x'
     };
+    window.dispatchEvent(new CustomEvent('versionsLoaded', { detail: window.APP_VERSIONS }));
   });
